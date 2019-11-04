09f46edb1cc4cef44c091dd8554d89f73eeb361b
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-04 15:35:52 +0000

    Completed the function that initialises the scoring matrix

--- dynprog.py
+++ dynprog.py
@@ -8,26 +8,21 @@ def populateScoringMatrix(alphabet, subMat, a, b):
     # Initialise the scoring matrix 
     scoMat = initialiseScoringMatrix(alphabet, subMat, a, b)
 
-   # for i in range(1, len(a)):
-      #  for j in range(1, len(b)):
-         #   bestScore = max(scoMat[i-1][j-1], scoMat[i-1][j], scoMat[i][j-1])
+    for i in range(1, len(a)):
+        for j in range(1, len(b)):
+            bestScore = max(scoMat[i-1][j-1], scoMat[i-1][j], scoMat[i][j-1])
 
 def initialiseScoringMatrix(alphabet, subMat, a, b):
 
     scoringMatrix = [[' ' for x in range(len(a) + 1)] for y in range(len(b) + 1)]
-    for i in range(0, len(b)):
-        print(scoringMatrix[i])
     scoringMatrix[0][0] = 0
 
     for x in range(1, len(b) + 1):
-        subMat[len(alphabet)][alphabet.index(b[x - 1])]
         scoringMatrix[x][0] = scoringMatrix[x-1][0] + subMat[len(alphabet)][alphabet.index(b[x - 1])]
-    for y in range(1, len(a)):
+    for y in range(1, len(a) + 1):
         scoringMatrix[0][y] = scoringMatrix[0][y-1] + subMat[len(alphabet)][alphabet.index(a[y - 1])]
     
-    print(scoringMatrix)
-    print()
-    for i in range(0, len(b) + 1):
+    for i in range(0, len(scoringMatrix)):
         print(scoringMatrix[i])
 
     return scoringMatrix
