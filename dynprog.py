d7aec19530c3f62fac702331ab3d94261827efb4
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-05 16:48:36 +0000

    Added a printMatrix() function and updated return values to return correct data

--- dynprog.py
+++ dynprog.py
@@ -5,28 +5,24 @@ def dynprof(alphabet, subMat, a, b):
     scoMat = lst[0]
     dirMat = lst[1]
     result = getBestMatching(scoMat, dirMat, a, b)
-    for i in range(0, len(scoMat)):
-        print(scoMat[i])
-    for i in range(0, len(dirMat)):
-        print(dirMat[i])
+    printMatrix(scoMat)
+    printMatrix(dirMat)
+    print(result)
+    return result
 
 def populateScoringMatrix(alphabet, subMat, a, b):
     scoMat = initialiseScoringMatrix(alphabet, subMat, a, b)
     dirMat = initialiseDirectionMatrix(alphabet, subMat, a, b)
     for i in range(1, len(a) + 1):
         for j in range(1, len(b) + 1):
-
             diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[i - 1])][alphabet.index(b[j - 1])]
             up = scoMat[i-1][j] + subMat[len(alphabet)][alphabet.index(a[i - 1])]
             left = scoMat[i][j-1] + subMat[alphabet.index(b[j - 1])][len(alphabet)]
             bestScore = max(diagonal, left, up)
-
             if bestScore == diagonal: dirMat[i][j] = "D"
             elif bestScore == up: dirMat[i][j] = "U"
             else: dirMat[i][j] = "L"
-
             scoMat[i][j] = bestScore
-
     return [scoMat, dirMat]
 
 def initialiseScoringMatrix(alphabet, subMat, a, b):
@@ -49,34 +45,33 @@ def initialiseDirectionMatrix(alphabet, subMat, a, b):
 def getBestMatching(scoMat, dirMat, a, b):
     xPos = len(a)
     yPos = len(b)
-
     aMatch = []
     bMatch = []
-
     while(xPos != 0 and yPos != 0):
         if(dirMat[xPos][yPos] == "D"):
-            aMatch = [yPos - 1] + aMatch
-            bMatch = [xPos - 1] + bMatch
+            aMatch = [xPos - 1] + aMatch
+            bMatch = [yPos - 1] + bMatch
             yPos -= 1
             xPos -= 1
         elif(dirMat[xPos][yPos] == "U"):
             xPos -= 1
         else:
             yPos -= 1
-    print(aMatch)
-    print(bMatch)
+    return [aMatch, bMatch]
+
+
+def printMatrix(matrix):
+    for i in range(len(matrix)):
+        print(matrix[i])
 
 
 
-
+#Examples 
 #a = [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]]
 #dynprof("ABC", a, "ABCACA", "BAACB")
 
-print()
-
-#Example from slides
 #b = [[1,-1,-1,-1,-2],[-1,1,-1,-1,-2],[-1,-1,1,-1,-2],[-1,-1,-1,1,-2],[-2,-2,-2,-2,1]]
 #dynprof("ACGT", b, "AAAC", "AGC") 
 
-c = [[1,-1,-1,-1,-1],[-1,1,-1,-1,-1],[-1,-1,1,-1,-1],[-1,-1,-1,1,-1],[-1,-1,-1,-1,1]]
-dynprof("ACGT", c, "GACTTAC", "CGTGAATTCAT") 
+#c = [[1,-1,-1,-1,-1],[-1,1,-1,-1,-1],[-1,-1,1,-1,-1],[-1,-1,-1,1,-1],[-1,-1,-1,-1,1]]
+#dynprof("ACGT", c, "GACTTAC", "CGTGAATTCAT") 
