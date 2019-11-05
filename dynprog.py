6d487bb46248f2a39c05ca924b7f9748753e7ae5
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-05 16:13:36 +0000

    Fixed some issues in producing correct results + updated position of string 1 and string 2 in tables

--- dynprog.py
+++ dynprog.py
@@ -4,60 +4,51 @@ def dynprof(alphabet, subMat, a, b):
     lst = populateScoringMatrix(alphabet, subMat, a, b)
     scoMat = lst[0]
     dirMat = lst[1]
-
     result = getBestMatching(scoMat, dirMat, a, b)
-
     for i in range(0, len(scoMat)):
         print(scoMat[i])
-
     for i in range(0, len(dirMat)):
         print(dirMat[i])
 
 def populateScoringMatrix(alphabet, subMat, a, b):
     scoMat = initialiseScoringMatrix(alphabet, subMat, a, b)
     dirMat = initialiseDirectionMatrix(alphabet, subMat, a, b)
-    for i in range(1, len(b) + 1):
-        for j in range(1, len(a) + 1):
-            if(j - 1 == 0 and i - 1 == 0): diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[j - 1])][alphabet.index(b[i - 1])]
-            elif (i - 1 == 0): diagonal = scoMat[i-1][j-1] + subMat[len(alphabet)][alphabet.index(b[i - 1])]
-            elif (j - 1 == 0): diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[j - 1])][len(alphabet)]
-            else: diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[j - 1])][alphabet.index(b[i - 1])]
-            left = scoMat[i][j-1] + subMat[alphabet.index(a[j - 1])][len(alphabet)]
-            up = scoMat[i-1][j] + subMat[len(alphabet)][alphabet.index(b[i - 1])]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
 
-            if(i == 1 and j == 1):
-                print(alphabet.index(a[j - 1]))
-                print(alphabet.index(b[i - 1]))
-                print(subMat[alphabet.index(a[j - 1])][alphabet.index(b[i - 1])])
-                print(diagonal)
-
+            diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[i - 1])][alphabet.index(b[j - 1])]
+            up = scoMat[i-1][j] + subMat[len(alphabet)][alphabet.index(a[i - 1])]
+            left = scoMat[i][j-1] + subMat[alphabet.index(b[j - 1])][len(alphabet)]
             bestScore = max(diagonal, left, up)
-            scoMat[i][j] = bestScore
+
             if bestScore == diagonal: dirMat[i][j] = "D"
             elif bestScore == up: dirMat[i][j] = "U"
             else: dirMat[i][j] = "L"
+
+            scoMat[i][j] = bestScore
+
     return [scoMat, dirMat]
 
 def initialiseScoringMatrix(alphabet, subMat, a, b):
-    scoringMatrix = [[' ' for x in range(len(a) + 1)] for y in range(len(b) + 1)]
+    scoringMatrix = [[' ' for x in range(len(b) + 1)] for y in range(len(a) + 1)]
     scoringMatrix[0][0] = 0
-    for x in range(1, len(b) + 1):
-        scoringMatrix[x][0] = scoringMatrix[x-1][0] + subMat[len(alphabet)][alphabet.index(b[x - 1])]
-    for y in range(1, len(a) + 1):
-        scoringMatrix[0][y] = scoringMatrix[0][y-1] + subMat[len(alphabet)][alphabet.index(a[y - 1])]
+    for x in range(1, len(a) + 1):
+        scoringMatrix[x][0] = scoringMatrix[x-1][0] + subMat[len(alphabet)][alphabet.index(a[x - 1])]   
+    for y in range(1, len(b) + 1):
+        scoringMatrix[0][y] = scoringMatrix[0][y-1] + subMat[len(alphabet)][alphabet.index(b[y - 1])]
     return scoringMatrix
 
 def initialiseDirectionMatrix(alphabet, subMat, a, b):
-    directionMatrix = [[' ' for x in range(len(a) + 1)] for y in range(len(b) + 1)]
-    for x in range(1, len(b) + 1):
+    directionMatrix = [[' ' for x in range(len(b) + 1)] for y in range(len(a) + 1)]
+    for x in range(1, len(a) + 1):
         directionMatrix[x][0] = "U"
-    for y in range(1, len(a) + 1):
+    for y in range(1, len(b) + 1):
         directionMatrix[0][y] = "L"
     return directionMatrix
 
 def getBestMatching(scoMat, dirMat, a, b):
-    xPos = len(b)
-    yPos = len(a)
+    xPos = len(a)
+    yPos = len(b)
 
     aMatch = []
     bMatch = []
@@ -78,14 +69,14 @@ def getBestMatching(scoMat, dirMat, a, b):
 
 
 
-a = [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]]
-dynprof("ABC", a, "ABCACA", "BAACB")
+#a = [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]]
+#dynprof("ABC", a, "ABCACA", "BAACB")
 
 print()
 
 #Example from slides
 #b = [[1,-1,-1,-1,-2],[-1,1,-1,-1,-2],[-1,-1,1,-1,-2],[-1,-1,-1,1,-2],[-2,-2,-2,-2,1]]
-#dynprof("ACGT", b, "AGC", "AAAC") 
+#dynprof("ACGT", b, "AAAC", "AGC") 
 
-#c = [[2,-2,-2,-1,-1],[-2,2,-2,-2,-1],[-2,-2,2,-2,-1],[-2,-2,-2,2,-1],[-1,-1,-1,-1,2]]
-#dynprof("ACGT", c, "CTCTGAG", "TGTCAGT") 
+c = [[1,-1,-1,-1,-1],[-1,1,-1,-1,-1],[-1,-1,1,-1,-1],[-1,-1,-1,1,-1],[-1,-1,-1,-1,1]]
+dynprof("ACGT", c, "GACTTAC", "CGTGAATTCAT") 
