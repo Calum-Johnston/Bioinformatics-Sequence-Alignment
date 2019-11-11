b74826bbb1cb0516a2c233fdcf83eaea7d146148
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-11 19:27:26 +0000

    Re-formatted some code

--- dynprog.py
+++ dynprog.py
@@ -2,23 +2,16 @@
 
 def dynprog(alphabet, subMat, a, b):
     lst = populateScoringMatrix(alphabet, subMat, a, b)
-    scoMat = lst[0]
-    dirMat = lst[1]
-    maxValue = lst[2]
-    maxValuePosition = lst[3]
+    scoMat = lst[0]; dirMat = lst[1]; maxValue = lst[2]; maxValuePosition = lst[3]
     alignment = getBestMatching(scoMat, dirMat, a, b, maxValuePosition)
     scoreAndAlignment = [maxValue, alignment[0], alignment[1]]
-    printMatrix(scoMat)
-    printMatrix(dirMat)
     return scoreAndAlignment
 
 def populateScoringMatrix(alphabet, subMat, a, b):
     scoMat = initialiseScoringMatrix(alphabet, subMat, a, b)
     dirMat = initialiseDirectionMatrix(alphabet, subMat, a, b)
-
     maxValue = 0
     maxValuePosition = [0, 0]
-
     for i in range(1, len(a) + 1):
         for j in range(1, len(b) + 1):
             diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[i - 1])][alphabet.index(b[j - 1])]
@@ -28,12 +21,9 @@ def populateScoringMatrix(alphabet, subMat, a, b):
             if bestScore == diagonal: dirMat[i][j] = "D"
             elif bestScore == up: dirMat[i][j] = "U"
             elif bestScore == left: dirMat[i][j] = "L"
-            if(bestScore > maxValue):
-                maxValue = bestScore
-                maxValuePosition[0] = i
-                maxValuePosition[1] = j
             scoMat[i][j] = bestScore
-    print(maxValue)
+            if(bestScore > maxValue):
+                maxValue = bestScore; maxValuePosition[0] = i; maxValuePosition[1] = j
     return [scoMat, dirMat, maxValue, maxValuePosition]
 
 def initialiseScoringMatrix(alphabet, subMat, a, b):
@@ -54,24 +44,19 @@ def initialiseDirectionMatrix(alphabet, subMat, a, b):
     return directionMatrix
 
 def getBestMatching(scoMat, dirMat, a, b, maxValuePos):
-    xPos = maxValuePos[0]
-    yPos = maxValuePos[1]
-    aMatch = []
-    bMatch = []
+    xPos = maxValuePos[0]; yPos = maxValuePos[1]
+    aMatch = []; bMatch = []
     while(scoMat[xPos][yPos] != 0):
         if(dirMat[xPos][yPos] == "D"):
             aMatch = [xPos - 1] + aMatch
             bMatch = [yPos - 1] + bMatch
-            yPos -= 1
-            xPos -= 1
+            xPos -= 1 ; yPos -= 1
         elif(dirMat[xPos][yPos] == "U"):
             xPos -= 1
         else:
             yPos -= 1
     
     return [aMatch, bMatch]    
-
-
 
 def printMatrix(matrix):
     for i in range(len(matrix)):
@@ -93,6 +78,6 @@ a = dynprog ("ABC", [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]], "AABB
 print("Score:   ", a[0])
 print("Indices: ", a[1],a[2])
 
-#b = dynprog ("ACT", [[1,-1,-1,-2],[-1,1,-1,-2],[-1,-1,1,-2],[-2,-2,-2,1]], "TAATA", "TACTAA")
-#print("Score:   ", b[0])
-#print("Indices: ", b[1],b[2])
+b = dynprog ("ACT", [[1,-1,-1,-2],[-1,1,-1,-2],[-1,-1,1,-2],[-2,-2,-2,1]], "TAATA", "TACTAA")
+print("Score:   ", b[0])
+print("Indices: ", b[1],b[2])
