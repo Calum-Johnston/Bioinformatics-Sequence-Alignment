4fd332b5ac5810b45148cf3f5065fa35cdf27443
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-11 19:03:01 +0000

    Updated initialiseScoringMatrix() to work with local alignment

--- dynprog.py
+++ dynprog.py
@@ -19,10 +19,10 @@ def populateScoringMatrix(alphabet, subMat, a, b):
             diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[i - 1])][alphabet.index(b[j - 1])]
             up = scoMat[i-1][j] + subMat[len(alphabet)][alphabet.index(a[i - 1])]
             left = scoMat[i][j-1] + subMat[alphabet.index(b[j - 1])][len(alphabet)]
-            bestScore = max(diagonal, left, up)
+            bestScore = max(diagonal, left, up, 0)
             if bestScore == diagonal: dirMat[i][j] = "D"
             elif bestScore == up: dirMat[i][j] = "U"
-            else: dirMat[i][j] = "L"
+            elif bestScore == left: dirMat[i][j] = "L"
             scoMat[i][j] = bestScore
     return [scoMat, dirMat]
 
@@ -30,9 +30,9 @@ def initialiseScoringMatrix(alphabet, subMat, a, b):
     scoringMatrix = [[' ' for x in range(len(b) + 1)] for y in range(len(a) + 1)]
     scoringMatrix[0][0] = 0
     for x in range(1, len(a) + 1):
-        scoringMatrix[x][0] = scoringMatrix[x-1][0] + subMat[len(alphabet)][alphabet.index(a[x - 1])]   
+        scoringMatrix[x][0] = max(0, scoringMatrix[x-1][0] + subMat[len(alphabet)][alphabet.index(a[x - 1])])   
     for y in range(1, len(b) + 1):
-        scoringMatrix[0][y] = scoringMatrix[0][y-1] + subMat[len(alphabet)][alphabet.index(b[y - 1])]
+        scoringMatrix[0][y] = max(0, scoringMatrix[0][y-1] + subMat[len(alphabet)][alphabet.index(b[y - 1])])
     return scoringMatrix
 
 def initialiseDirectionMatrix(alphabet, subMat, a, b):
@@ -76,3 +76,11 @@ def printMatrix(matrix):
 
 #c = [[1,-1,-1,-1,-1],[-1,1,-1,-1,-1],[-1,-1,1,-1,-1],[-1,-1,-1,1,-1],[-1,-1,-1,-1,1]]
 #dynprog("ACGT", c, "GACTTAC", "CGTGAATTCAT") 
+
+a = dynprog ("ABC", [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]], "AABBAACA", "CBACCCBA")
+print("Score:   ", a[0])
+print("Indices: ", a[1],a[2])
+
+b = dynprog ("ACT", [[1,-1,-1,-2],[-1,1,-1,-2],[-1,-1,1,-2],[-2,-2,-2,1]], "TAATA", "TACTAA")
+print("Score:   ", b[0])
+print("Indices: ", b[1],b[2])
