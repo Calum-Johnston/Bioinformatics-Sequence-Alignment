60f3fc6033f042b70cc676bc70e4052e1d715638
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-17 14:15:01 +0000

    Began work on implementing FASTA as a heuristic approach to solving local alignment

---
+++ toSubmit/heuralign.py
@@ -0,0 +1,7 @@
+def heuralign(alphabet, subMat, a, ):
+    print("Hi")
+
+
+#a = dynprog ("ABCD", [[1,-5,-5,-5,-1],[-5, 1,-5,-5,-1],[-5,-5, 5,-5,-4],[-5,-5,-5, 6,-4],[-1,-1,-4,-4,-9]], "AAAAACCDDCCDDAAAAACC", "CCAAADDAAAACCAAADDCCAAAA")
+#print("Score:   ", a[0])
+#print("Indices: ", a[1],a[2])
