fabd523b3296a083acfc975d74cab772197e1cbe
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-18 12:18:41 +0000

    Added Smith-Waterson Algorithm
    It's for later use, in calculating the local alignment based on a limited section of the scoring matrix

--- toSubmit/heuralign.py
+++ toSubmit/heuralign.py
@@ -4,7 +4,8 @@ import itertools
 def heuralign(alphabet, subMat, a, b, ktup):
     indexTable = initialiseIndexTable(alphabet, a, ktup)
     matches = getMatches(indexTable, a, b, ktup)
-    orderPairs(matches, a, b)
+    diagonalPairs = orderPairs(matches, a, b)
+    #scoreDiagonals(diagonalPairs)
 
 def initialiseIndexTable(alphabet, a, ktup):
     keywords = [''.join(i) for i in itertools.product("ABCD", repeat = 2)]
@@ -34,8 +35,73 @@ def orderPairs(matches, a, b):
         lst = diagonalPairs[(match[1]-match[0])]
         lst.append(match)
         diagonalPairs[(match[1]-match[0])] = lst
-    for key in diagonalPairs:
-        print("{}: {}".format(key, diagonalPairs[key]))
+    return diagonalPairs
+
+def scoreDiagonals(diagonalPairs):
+    return 0
+
+
+
+# SMITH-WATERSON 
+# Needs updating to limit matrix space used
+def dynprog(alphabet, subMat, a, b):
+    lst = populateScoringMatrix(alphabet, subMat, a, b)
+    scoMat = lst[0]; dirMat = lst[1]; maxValue = lst[2]; maxValuePosition = lst[3]
+    alignment = getBestMatching(scoMat, dirMat, a, b, maxValuePosition)
+    scoreAndAlignment = [maxValue, alignment[0], alignment[1]]
+    return scoreAndAlignment
+
+def populateScoringMatrix(alphabet, subMat, a, b):
+    scoMat = initialiseScoringMatrix(alphabet, subMat, a, b)
+    dirMat = initialiseDirectionMatrix(alphabet, subMat, a, b)
+    maxValue = 0
+    maxValuePosition = [0, 0]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[i - 1])][alphabet.index(b[j - 1])]
+            up = scoMat[i-1][j] + subMat[len(alphabet)][alphabet.index(a[i - 1])]
+            left = scoMat[i][j-1] + subMat[alphabet.index(b[j - 1])][len(alphabet)]
+            bestScore = max(diagonal, left, up, 0)
+            if bestScore == diagonal: dirMat[i][j] = "D"
+            elif bestScore == up: dirMat[i][j] = "U"
+            elif bestScore == left: dirMat[i][j] = "L"
+            scoMat[i][j] = bestScore
+            if(bestScore > maxValue):
+                maxValue = bestScore; maxValuePosition[0] = i; maxValuePosition[1] = j
+    return [scoMat, dirMat, maxValue, maxValuePosition]
+
+def initialiseScoringMatrix(alphabet, subMat, a, b):
+    scoringMatrix = [[' ' for x in range(len(b) + 1)] for y in range(len(a) + 1)]
+    scoringMatrix[0][0] = 0
+    for x in range(1, len(a) + 1):
+        scoringMatrix[x][0] = max(0, scoringMatrix[x-1][0] + subMat[len(alphabet)][alphabet.index(a[x - 1])])   
+    for y in range(1, len(b) + 1):
+        scoringMatrix[0][y] = max(0, scoringMatrix[0][y-1] + subMat[len(alphabet)][alphabet.index(b[y - 1])])
+    return scoringMatrix
+
+def initialiseDirectionMatrix(alphabet, subMat, a, b):
+    directionMatrix = [[' ' for x in range(len(b) + 1)] for y in range(len(a) + 1)]
+    for x in range(1, len(a) + 1):
+        directionMatrix[x][0] = "U"        
+    for y in range(1, len(b) + 1):
+        directionMatrix[0][y] = "L"
+    return directionMatrix
+
+def getBestMatching(scoMat, dirMat, a, b, maxValuePos):
+    xPos = maxValuePos[0]; yPos = maxValuePos[1]
+    aMatch = []; bMatch = []
+    while(scoMat[xPos][yPos] != 0):
+        if(dirMat[xPos][yPos] == "D"):
+            aMatch = [xPos - 1] + aMatch
+            bMatch = [yPos - 1] + bMatch
+            xPos -= 1 ; yPos -= 1
+        elif(dirMat[xPos][yPos] == "U"):
+            xPos -= 1
+        else:
+            yPos -= 1
+    return [aMatch, bMatch]    
+
+
 
 #a = heuralign ("ABCD", [[1,-5,-5,-5,-1],[-5, 1,-5,-5,-1],[-5,-5, 5,-5,-4],[-5,-5,-5, 6,-4],[-1,-1,-4,-4,-9]], "ACDA", "AB", 2)
 a = heuralign ("ABCD", [[1,-5,-5,-5,-1],[-5, 1,-5,-5,-1],[-5,-5, 5,-5,-4],[-5,-5,-5, 6,-4],[-1,-1,-4,-4,-9]], "AAAAACCDDCCDDAAAAACC", "CCAAADDAAAACCAAADDCCAAAA", 2)
