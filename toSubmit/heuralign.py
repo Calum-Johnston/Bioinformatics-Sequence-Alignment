bd45797299d703fc3a2df26053f6cfb69898ed98
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-29 12:08:42 +0000

    Working on implementing bandedDP

--- toSubmit/heuralign.py
+++ toSubmit/heuralign.py
@@ -5,7 +5,7 @@ def heuralign(alphabet, subMat, a, b, ktup):
     indexTable = initialiseIndexTable(alphabet, a, ktup)
     matches = getMatches(indexTable, a, b, ktup)
     diagonalPairs = orderPairs(matches, a, b)
-    scoreDiagonals(diagonalPairs)
+    scoreDiagonals(diagonalPairs, subMat, alphabet, a, b)
 
 def initialiseIndexTable(alphabet, a, ktup):
     keywords = [''.join(i) for i in itertools.product("ABCD", repeat = 2)]
@@ -37,61 +37,67 @@ def orderPairs(matches, a, b):
         diagonalPairs[(match[1]-match[0])] = lst
     return diagonalPairs
 
-def scoreDiagonals(diagonalPairs):
-    bestDiagonals = []
-    totalScore = 0
-    for diagonals in diagonalPairs.values():
-        startPos = []
-        endPos = []
-        value = 0
-        for dia in diagonals:
-            startPos = 
+def scoreDiagonals(diagonalPairs, subMat, alphabet, a, b):
+    x,y = 0
+    for diagonal,diagonalValues in diagonalPairs.items():
+        print(diagonal)
+        print(diagonalValues)
+        if(diagonal > 0):
+            x = 0
+        currentAlign = []
+        currentScore = 0
+        bestAlign = []
+        bestScore = 0
+        #for dia in diagonals:
+           # if(len(dia) != 0):  # Ignore diagonals with no matches
+            #    score = subMat[alphabet.index(a[dia[0]])][alphabet.index(b[dia[1]])] + subMat[alphabet.index(a[dia[0] + 1])][alphabet.index(b[dia[1] + 1])]
+                
             
 
 
 
 # SMITH-WATERSON 
 # Needs updating to limit matrix space used
-def dynprog(alphabet, subMat, a, b):
-    lst = populateScoringMatrix(alphabet, subMat, a, b)
+def dynprog(alphabet, subMat, a, b, diagonalL, diagonalR):
+    lst = populateScoringMatrix(alphabet, subMat, a, b, diagonalL, diagonalR)
     scoMat = lst[0]; dirMat = lst[1]; maxValue = lst[2]; maxValuePosition = lst[3]
     alignment = getBestMatching(scoMat, dirMat, a, b, maxValuePosition)
     scoreAndAlignment = [maxValue, alignment[0], alignment[1]]
     return scoreAndAlignment
 
-def populateScoringMatrix(alphabet, subMat, a, b):
-    scoMat = initialiseScoringMatrix(alphabet, subMat, a, b)
-    dirMat = initialiseDirectionMatrix(alphabet, subMat, a, b)
+def populateScoringMatrix(alphabet, subMat, a, b, diagonalX, diagonalY):
+    scoMat = initialiseScoringMatrix(alphabet, subMat, a, b, diagonalX, diagonalY)
+    dirMat = initialiseDirectionMatrix(alphabet, subMat, a, b, diagonalX, diagonalY)
     maxValue = 0
     maxValuePosition = [0, 0]
-    for i in range(1, len(a) + 1):
-        for j in range(1, len(b) + 1):
-            diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[i - 1])][alphabet.index(b[j - 1])]
-            up = scoMat[i-1][j] + subMat[len(alphabet)][alphabet.index(a[i - 1])]
-            left = scoMat[i][j-1] + subMat[alphabet.index(b[j - 1])][len(alphabet)]
-            bestScore = max(diagonal, left, up, 0)
-            if bestScore == diagonal: dirMat[i][j] = "D"
-            elif bestScore == up: dirMat[i][j] = "U"
-            elif bestScore == left: dirMat[i][j] = "L"
-            scoMat[i][j] = bestScore
-            if(bestScore > maxValue):
-                maxValue = bestScore; maxValuePosition[0] = i; maxValuePosition[1] = j
+    currentX, currentY = 0
+    while(currentX < len(a) + 1):
+        diagonal = scoMat[i-1][j-1] + subMat[alphabet.index(a[i - 1])][alphabet.index(b[j - 1])]
+        up = scoMat[i-1][j] + subMat[len(alphabet)][alphabet.index(a[i - 1])]
+        left = scoMat[i][j-1] + subMat[alphabet.index(b[j - 1])][len(alphabet)]
+        bestScore = max(diagonal, left, up, 0)
+        if bestScore == diagonal: dirMat[i][j] = "D"
+        elif bestScore == up: dirMat[i][j] = "U"
+        elif bestScore == left: dirMat[i][j] = "L"
+        scoMat[i][j] = bestScore
+        if(bestScore > maxValue):
+            maxValue = bestScore; maxValuePosition[0] = i; maxValuePosition[1] = j
     return [scoMat, dirMat, maxValue, maxValuePosition]
 
-def initialiseScoringMatrix(alphabet, subMat, a, b):
+def initialiseScoringMatrix(alphabet, subMat, a, b, diagonalX, diagonalY):
     scoringMatrix = [[' ' for x in range(len(b) + 1)] for y in range(len(a) + 1)]
     scoringMatrix[0][0] = 0
-    for x in range(1, len(a) + 1):
+    for x in range(1, abs(diagonalX)):
         scoringMatrix[x][0] = max(0, scoringMatrix[x-1][0] + subMat[len(alphabet)][alphabet.index(a[x - 1])])   
-    for y in range(1, len(b) + 1):
+    for y in range(1, diagonalY):
         scoringMatrix[0][y] = max(0, scoringMatrix[0][y-1] + subMat[len(alphabet)][alphabet.index(b[y - 1])])
     return scoringMatrix
 
-def initialiseDirectionMatrix(alphabet, subMat, a, b):
+def initialiseDirectionMatrix(alphabet, subMat, a, b, diagonalX, diagonalY):
     directionMatrix = [[' ' for x in range(len(b) + 1)] for y in range(len(a) + 1)]
-    for x in range(1, len(a) + 1):
+    for x in range(1, abs(diagonalX)):
         directionMatrix[x][0] = "U"        
-    for y in range(1, len(b) + 1):
+    for y in range(1, diagonalY):
         directionMatrix[0][y] = "L"
     return directionMatrix
 
@@ -111,7 +117,7 @@ def getBestMatching(scoMat, dirMat, a, b, maxValuePos):
 
 
 
-#a = heuralign ("ABCD", [[1,-5,-5,-5,-1],[-5, 1,-5,-5,-1],[-5,-5, 5,-5,-4],[-5,-5,-5, 6,-4],[-1,-1,-4,-4,-9]], "ACDA", "AB", 2)
-a = heuralign ("ABCD", [[1,-5,-5,-5,-1],[-5, 1,-5,-5,-1],[-5,-5, 5,-5,-4],[-5,-5,-5, 6,-4],[-1,-1,-4,-4,-9]], "AAAAACCDDCCDDAAAAACC", "CCAAADDAAAACCAAADDCCAAAA", 2)
+#a = heuralign ("ABCD", [[1,-5,-5,-5,-1],[-5, 1,-5,-5,-1],[-5,-5, 5,-5,-4],[-5,-5,-5, 6,-4],[-1,-1,-4,-4,-9]], "ABDAAB", "AB", 2)
+a = heuralign ("ABCD", [[1,-5,-5,-5,-1],[-5, 1,-5,-5,-1],[-5,-5, 5,-5,-4],[-5,-5,-5, 6,-4],[-1,-1,-4,-4,-9]], "AAAAACCDDCCDDAAAAACC4", "CCAAADDAAAACCAAADDCCAAAA", 2)
 #print("Score:   ", a[0])
 #print("Indices: ", a[1],a[2])
